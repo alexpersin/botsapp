97e1c2c74f023e3aaa72e9f4b8bc85166c5ed9ac
Lewis Wilkins <lewiswilkins@me.com>
2018-10-22 21:28:48 +0200

    Incorperated Alex's comments. Also added some functionality to SlackAdapter - now sender is the display name of sender rather than the user id.

--- experiments/slack-events-api-test/Message.py
+++ experiments/slack-events-api-test/Message.py
@@ -1,17 +1,16 @@
 
 class Message():
-    def __init__(self, timestamp):
-        self.timestamp = timestamp
-        self.message_content = ''
-        self.message_sender  = ''
-        self.message_recipient = ''
-
-    def set_message_content(self, message_sender, message_recipient, message_content):
-        self.message_content = message_content
-        self.message_sender  = message_sender
+    def __init__(self, timestamp, message_sender, message_recipient, message_content):
+        self.timestamp         = timestamp
+        self.message_content   = message_content
+        self.message_sender    = message_sender
         self.message_recipient = message_recipient
 
-    def print_message(self):
-        print('''Message:\n \
-                 From: %s\n \
-                 Content %s''' % (self.message_sender, self.message_content))
+    def __str__(self):
+        return '''Message:
+        From: %s
+        Content: %s ''' % (self.message_sender, self.message_content)
+    
+    def __repr__(self):
+        return 'Message(From: %s, Content: %s)' % (self.message_sender, self.message_content)
+    
--- experiments/slack-events-api-test/SlackAdapter.py
+++ experiments/slack-events-api-test/SlackAdapter.py
@@ -3,41 +3,62 @@ from slackclient import SlackClient
 import os
 from Message import Message
 
-'''Basic slack adapater class. On init it expects you to have defined the SLACK_SIGNING_SECRET and SLACK_BOT_TOKEN.
-   I will show you where to find these'''
+
 
 class SlackAdapter():
+    '''Basic slack adapater class. On init it expects you to have defined the SLACK_SIGNING_SECRET and SLACK_BOT_TOKEN.
+       I will show you where to find these.'''
 
     def send_message(self, channel, message):
         self.slack_client.api_call("chat.postMessage", channel=channel, text=message)
+    
+    def get_display_name(self, user_id):
+        users_list = self.client.api_call("users.list")
+        if users_list["ok"]:
+            members = users_list["members"]
+            for member in members:
+                if member["id"] == user_id:
+                    return member["profile"]["display_name"]
+                
+            return "Could not find user id %" % user_id
+        else:
+            return "Could not get users list"
 
-    def _slack_message_conversion(self, message):
-        botsapp_message = Message(message["event_ts"])
-        botsapp_message.set_message_content(message["user"], "", message["text"])
+    def _message_conversion(self, message):
+        botsapp_message = Message(message["event_ts"], self.get_display_name(message["user"]), "", message["text"])
         return botsapp_message
 
     
-    
-    def __init__(self):
-        self.slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-        self.slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+    def __init__(self, port=3000):
+        self.signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+        self.bot_token = os.environ["SLACK_BOT_TOKEN"]
         
-        self.slack_events_adapter = SlackEventAdapter(self.slack_signing_secret, "/slack/events")
-        self.slack_client = SlackClient(self.slack_bot_token)
-
+        self.events_adapter = SlackEventAdapter(self.signing_secret, "/slack/events")
+        self.client = SlackClient(self.bot_token)
+        self.port = port
+        self.adapater_running = False
         '''Not sure how else to do this because of the @, Alex maybe you can explain what is going on but it seems
            I have to have this block of code here and not as a separate method. I've just set it to print out the
            message for now, I guess it is in here we can make it do whatever we want.'''
 
-        @self.slack_events_adapter.on("message")
+        @self.events_adapter.on("message")
         def handle_message(event_data):
             message = event_data["event"]
-            output_message = self._slack_message_conversion(message)
-            output_message.print_message()
-        
-        self.slack_events_adapter.start(port=3000)
-        
+            output_message = self._message_conversion(message)
+            print(output_message)
+    
+    def __str__(self):
+        return "SlackAdapter(port: %i )" % (self.port)
 
+    def __repr__(self):
+        return "SlackAdapter(port: %i )" % (self.port)
+    
+    def start_adapter(self, port=3000):
+        self.port = port
+        self.events_adapter.start(port=port)
+
+        
+    
 
 
 
